85e4ead57557b30f529d74b3809df63d2f54bd1c
Leo Suarez <Leo@DTRaleigh.com>
2016-09-04 20:42:30 -0400

    Created models. Add bootstrap.

--- capitals/admin.py
+++ capitals/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from capitals.models import Photo, Capital
 
-# Register your models here.
+class photo_admin(admin.ModelAdmin):
+    list_display = ('name', 'photo_file')
+
+class capital_admin(admin.ModelAdmin):
+    list_display = ('name', 'us_state', 'date_visited', 'lat', 'lon')
+
+admin.site.register(Photo, photo_admin)
+admin.site.register(Capital, capital_admin)
--- capitals/models.py
+++ capitals/models.py
@@ -1,3 +1,129 @@
 from django.db import models
 
-# Create your models here.
+class Capital(models.Model):
+    ALABAMA	= 'AL'
+    ALASKA = 'AK'
+    ARIZONA = 'AZ'
+    ARKANSAS = 'AR'
+    CALIFORNIA = 'CA'
+    COLORADO = 'CO'
+    CONNECTICUT = 'CT'
+    DELAWARE = 'DE'
+    FLORIDA = 'FL'
+    GEORGIA = 'GA'
+    HAWAII = 'HI'
+    IDAHO = 'ID'
+    ILLINOIS = 'IL'
+    INDIANA = 'IN'
+    IOWA = 'IA'
+    KANSAS = 'KS'
+    KENTUCKY = 'KY'
+    LOUISIANA = 'LA'
+    MAINE = 'ME'
+    MARYLAND = 'MD'
+    MASSACHUSETTS = 'MA'
+    MICHIGAN = 'MI'
+    MINNESOTA = 'MN'
+    MISSISSIPPI = 'MS'
+    MISSOURI = 'MO'
+    MONTANA = 'MT'
+    NEBRASKA = 'NE'
+    NEVADA = 'NV'
+    NEW_HAMPSHIRE = 'NH'
+    NEW_JERSEY = 'NJ'
+    NEW_MEXICO = 'NM'
+    NEW_YORK = 'NY'
+    NORTH_CAROLINA = 'NC'
+    NORTH_DAKOTA = 'ND'
+    OHIO = 'OH'
+    OKLAHOMA = 'OK'
+    OREGON = 'OR'
+    PENNSYLVANIA = 'PA'
+    RHODE_ISLAND = 'RI'
+    SOUTH_CAROLINA = 'SC'
+    SOUTH_DAKOTA = 'SD'
+    TENNESSEE = 'TN'
+    TEXAS = 'TX'
+    UTAH = 'UT'
+    VERMONT	= 'VT'
+    VIRGINIA = 'VA'
+    WASHINGTON = 'WA'
+    WEST_VIRGINIA = 'WV'
+    WISCONSIN = 'WI'
+    WYOMING = 'WY'
+
+    STATE_CHOICES = (
+        (ALABAMA, 'Alabama'),
+        (ALASKA, 'Alaska'),
+        (ARIZONA, 'Arizona'),
+        (ARKANSAS, 'Arkansas'),
+        (CALIFORNIA, 'California'),
+        (COLORADO, 'Colorado'),
+        (CONNECTICUT, 'Connecticut'),
+        (DELAWARE, 'Delaware'),
+        (FLORIDA, 'Florida'),
+        (GEORGIA, 'Georgia'),
+        (HAWAII, 'Hawaii'),
+        (IDAHO, 'Idaho'),
+        (ILLINOIS, 'Illinois'),
+        (INDIANA, 'Indiana'),
+        (IOWA, 'Iowa'),
+        (KANSAS, 'Kansas'),
+        (KENTUCKY, 'Kentucky'),
+        (LOUISIANA, 'Louisiana'),
+        (MAINE, 'Maine'),
+        (MARYLAND, 'Maryland'),
+        (MASSACHUSETTS, 'Massachusetts'),
+        (MICHIGAN, 'Michigan'),
+        (MINNESOTA, 'Minnesota'),
+        (MISSISSIPPI, 'Mississippi'),
+        (MISSOURI, 'Missouri'),
+        (MONTANA, 'Montana'),
+        (NEBRASKA, 'Nebraska'),
+        (NEVADA, 'Nevada'),
+        (NEW_HAMPSHIRE, 'New Hampshire'),
+        (NEW_JERSEY, 'New Jersey'),
+        (NEW_MEXICO, 'New Mexico'),
+        (NEW_YORK, 'New York'),
+        (NORTH_CAROLINA, 'North Carolina'),
+        (NORTH_DAKOTA, 'North Dakota'),
+        (OHIO, 'Ohio'),
+        (OREGON, 'Oregon'),
+        (PENNSYLVANIA, 'Pennsylvania'),
+        (RHODE_ISLAND, 'Rhode Island'),
+        (SOUTH_CAROLINA, 'South_Carolina'),
+        (SOUTH_DAKOTA, 'South Dakota'),
+        (TENNESSEE, 'Tennessee'),
+        (TEXAS, 'Texas'),
+        (UTAH, 'Utah'),
+        (VERMONT, 'Vermont'),
+        (VIRGINIA, 'Virginia'),
+        (WASHINGTON, 'Washington'),
+        (WEST_VIRGINIA, 'West Virginia'),
+        (WISCONSIN, 'Wisconsin'),
+        (WYOMING, 'Wyoming'),
+    )
+
+    name = models.CharField(max_length=200, unique=True)
+    us_state = models.CharField(
+        max_length=2,
+        choices=STATE_CHOICES,
+        blank=True,
+        default=None,
+        verbose_name='US State',
+    )
+    date_visited = models.DateField()
+    description = models.TextField(blank=True)
+    lat = models.DecimalField(max_digits=9, decimal_places=6, default=0, verbose_name='Latitude')
+    lon = models.DecimalField(max_digits=9, decimal_places=6, default=0, verbose_name='Longitude')
+    photos = models.ManyToManyField('Photo', default=None, blank=True)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+class Photo(models.Model):
+    name = models.CharField(max_length=200)
+    photo_file = models.FileField(upload_to='photos/')
+
+    def __str__(self):
+        return '%s' % (self.name)
