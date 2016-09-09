1e4019ef620d062ea26d89cf550898f8aee37159
Leo Suarez <Leo@DTRaleigh.com>
2016-09-08 20:51:42 -0400

    Added Statistics progress bar for US capitals. Changed photo column number to 2 cause it looks better.

--- capitals/views.py
+++ capitals/views.py
@@ -7,6 +7,11 @@ def home(request):
     all_capitals = Capital.objects.all()
     all_photos = Photo.objects.all()
 
+    us_capitals_visited = us_capitals.count()
+    us_capitals_visited_percent = (us_capitals_visited / 50 ) * 100
+
     return render(request, 'index.html', {'us_capitals':us_capitals,
                                         'all_photos':all_photos,
-                                        'all_capitals':all_capitals})
+                                        'all_capitals':all_capitals,
+                                        'us_capitals_visited':us_capitals_visited,
+                                        'us_capitals_visited_percent':us_capitals_visited_percent})
