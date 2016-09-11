16277e542598ccc064a0ac862a147e33a8af0ee1
Leo Suarez <Leo@DTRaleigh.com>
2016-09-11 13:57:24 -0400

    Finished adding anchor links to capital list.

--- capitals/views.py
+++ capitals/views.py
@@ -6,6 +6,7 @@ from capitals.models import Capital, Photo
 def home(request):
     #Get some information to start with
     us_capitals = Capital.objects.exclude(us_state='')
+    other_capitals = Capital.objects.filter(us_state='')
     all_capitals = Capital.objects.all()
     all_photos = Photo.objects.all()
 
@@ -13,9 +14,10 @@ def home(request):
     us_capitals_visited = us_capitals.count()
     us_capitals_visited_percent = (us_capitals_visited / 50 ) * 100
 
-    #Get a list of all the capitals in the format ('city name', 0/1)
+    #Get a list of all the capitals in the format ('city name', 0/1, 'XX')
     #The city name is the name of the capital,
-    #0 is for not visited, 1 is for visited.
+    #0 is for not visited, 1 is for visited,
+    #'XX' is the state code.
     us_states_list = []
     us_states_choices = Capital._meta.get_field('us_state').choices
     for state in us_states_choices:
@@ -28,6 +30,8 @@ def home(request):
         else:
             capital.append(False)
 
+        capital.append(state[0])
+
         us_states_list.append(capital)
 
     return render(request, 'index.html', {'us_capitals':us_capitals,
@@ -35,4 +39,5 @@ def home(request):
                                         'all_capitals':all_capitals,
                                         'us_capitals_visited':us_capitals_visited,
                                         'us_capitals_visited_percent':us_capitals_visited_percent,
-                                        'us_states_list':us_states_list})
+                                        'us_states_list':us_states_list,
+                                        'other_capitals':other_capitals})
