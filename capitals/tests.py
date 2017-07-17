2eac3efca4b5634d8fbe083b7b81d0485f22817a
Leo Suarez <Leo@DTRaleigh.com>
2017-07-16 21:54:39 -0400

    cleanup a little on views, unused variables, and first test.

--- capitals/tests.py
+++ capitals/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from capitals.views import create_us_states_list
+
+class capitals_info_check(TestCase):
+
+    def test_output_info(self):
+        us_states_list = create_us_states_list()
+
+        print(us_states_list)
+
--- capitals/views.py
+++ capitals/views.py
@@ -3,6 +3,29 @@ import us
 from django.shortcuts import render
 from capitals.models import Capital, Photo
 
+def create_us_states_list():
+    # Get a list of all the capitals in the format ('city name', 0/1, 'XX')
+    # The city name is the name of the capital,
+    # 0 is for not visited, 1 is for visited,
+    # 'XX' is the state code.
+    us_states_list = []
+    us_states_choices = Capital._meta.get_field('us_state').choices
+
+    for state in us_states_choices:
+        capital = []
+        capital.append(us.states.lookup(state[0]).capital + ', ' + state[0])
+
+        if Capital.objects.filter(us_state=state[0]).exists():
+            capital.append(True)
+        else:
+            capital.append(False)
+
+        capital.append(state[0])
+
+        us_states_list.append(capital)
+
+    return us_states_list
+
 def home(request):
     #Get some information to start with
     us_capitals = Capital.objects.exclude(us_state='')
@@ -14,25 +37,7 @@ def home(request):
     us_capitals_visited = us_capitals.count()
     us_capitals_visited_percent = (us_capitals_visited / 50 ) * 100
 
-    #Get a list of all the capitals in the format ('city name', 0/1, 'XX')
-    #The city name is the name of the capital,
-    #0 is for not visited, 1 is for visited,
-    #'XX' is the state code.
-    us_states_list = []
-    us_states_choices = Capital._meta.get_field('us_state').choices
-    for state in us_states_choices:
-        capital = []
-        city = us.states.lookup(state[0]).capital
-        capital.append(us.states.lookup(state[0]).capital + ', ' + state[0])
-
-        if Capital.objects.filter(us_state=state[0]).exists():
-            capital.append(True)
-        else:
-            capital.append(False)
-
-        capital.append(state[0])
-
-        us_states_list.append(capital)
+    us_states_list = create_us_states_list()
 
     return render(request, 'index.html', {'us_capitals':us_capitals,
                                         'all_photos':all_photos,
